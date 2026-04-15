17c7eda61cfdf67e384ca4194221a5fc1d48d1bc
Ramsha Saleem <ramshasaleem25@gmail.com>
2026-04-15 14:11:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,24 +54,14 @@ const App = () => {
     }
   }
 
-  const loadTrendingMovies=async()=>{
-    try{
-      const movies= await getTrendingMovies();
-      setTrendingMovies(movies);
-    } catch(error){
-      console.error(`Error fetching movies: ${error}`);
-      
-    }
-  }
+
 
 
   useEffect(()=>{
     fetchMovies(debouncedSearchTerm); 
   },[debouncedSearchTerm] )
 
-  useEffect(() => {
-    loadTrendingMovies();
-  }, []);
+ 
 
   return (
     <main>
@@ -83,20 +73,7 @@ const App = () => {
          <h1> Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1> 
          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />      
       </header>
-      {trendingMovies.length > 0 && (
-          <section className="trending">
-            <h2>Trending Movies</h2>
-
-            <ul>
-              {trendingMovies.map((movie, index) => (
-                <li key={movie.$id}>
-                  <p>{index + 1}</p>
-                  <img src={movie.poster_url} alt={movie.title} />
-                </li>
-              ))}
-            </ul>
-          </section>
-        )}
+      
 
       <section className="all-movies">
         <h2 className="mt-[40px]" >All Movies</h2>
